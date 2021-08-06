2b83a06ee7497d1ce5f4f82dcc67a3ad62720bdf
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2021-08-05 20:20:02 -0400

    clients

--- site.js
+++ site.js
@@ -8,3 +8,35 @@ $hamburguer.addEventListener('click', function()
     $nav.classList.toggle("show")
     
 })
+
+document.addEventListener("DOMContentLoaded", function()
+{
+
+    fetch('http://localhost:3000/clients')
+    .then(response => response.json())
+    .then(function(data)
+    {
+
+        $clientes = document.querySelector("#clients .container")
+        $clientes.innerHTML = ""
+
+        for( var i=0; i < data.length; i++ )
+        {
+
+            $link = document.createElement("a")
+            $link.href = data[i].link
+            $link.title = data[i].title
+            $link.target = "_blank"
+
+            $img = document.createElement("img")
+            $img.src = "/img/"+data[i].image
+
+            $link.appendChild($img)
+
+            $clientes.appendChild($link)
+
+        }
+    });
+
+
+})
